a17c7be4003e4dcba8bacfdd82c74140e9ba3b7b
hodinhan10 <thichhinhhoc999@gmail.com>
2021-10-04 16:47:49 +0700

    make it better

--- backend/server.js
+++ backend/server.js
@@ -33,9 +33,9 @@ const __dirname = path.resolve();
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.use(express.static(path.join(__dirname, '/frontend/public')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/public/index.html'))
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
